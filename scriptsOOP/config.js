72ca68b2e869568c53f08445efb29da85a501c44
kwinkich <vankord21@gmail.com>
2023-03-10 19:45:50 +0300

    add opp version

---
+++ scriptsOOP/config.js
@@ -0,0 +1,9 @@
+export default class Config {
+
+    constructor() {
+        this.step = 0;
+        this.maxStep = 6;
+        this.sizeCell = 16;
+        this.sizeBarry = this.sizeCell / 4;
+    }
+}
